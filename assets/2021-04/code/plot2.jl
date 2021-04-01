2e0f95a5afce36b7ea842df6ad9b547a4623499b
hyrodium <hyrodium@gmail.com>
2021-04-01 12:08:14 +0000

    Deploying to gh-pages from  @ c25d3062b4bc7dfb0e5a38bacd6cf360e14e24ed 🚀

---
+++ assets/2021-04/code/plot2.jl
@@ -0,0 +1,6 @@
+# This file was generated, do not modify it. # hide
+import Plots
+gr()
+using LaTeXStrings
+Plots.plot([sin, cos], xlabel=L"x", ylabel=L"y")
+Plots.savefig(joinpath(@OUTPUT, "gr2.png")) # hide
